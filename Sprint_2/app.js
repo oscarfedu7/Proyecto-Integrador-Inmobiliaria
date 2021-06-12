f8c7b09eb9d517a3fa6b238f731e2aaf487f5f4d
mayrasillas <mayrasillas.c@hotmail.com>
2021-06-12 02:23:18 -0500

    Actualizacion 12/Junio

--- Sprint_2/app.js
+++ Sprint_2/app.js
@@ -6,8 +6,9 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3003, () => console.log('Servidor corriendo'));
+app.listen(3000, () => console.log('Servidor corriendo'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 })
+
