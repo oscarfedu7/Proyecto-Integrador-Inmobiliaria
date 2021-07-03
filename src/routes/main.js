48c2126ba2dafe69021a5619ea02f66941e51c87
mayrasillas <mayrasillas.c@hotmail.com>
2021-07-03 18:31:26 -0500

    index

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/mainController");
 
-//router.get("/", controller.index);
-router.get("/index", controller.index);
+router.get("/", controller.index);
+//router.get("/index", controller.index);
 router.get("/login", controller.login);
 router.get("/register", controller.register);
 
