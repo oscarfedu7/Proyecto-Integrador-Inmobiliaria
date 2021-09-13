89ad81ddfca8a72234604a574107e2eb31e6bbd3
mayrasillas <mayrasillas.c@hotmail.com>
2021-09-13 18:21:09 -0500

    databases

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,5 +1,27 @@
 module.exports = {
-  "development": {
+
+    "development": {
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
+    "dialect": "mysql"
+  }
+ /* "development": {
     "username": "root",
     "password": "mj*Cp-YwV9!d%BY",
     "database": "bits",
@@ -19,5 +41,5 @@ module.exports = {
     "database": "bits",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  }
+  }*/
 }
