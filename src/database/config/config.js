661fce54e207cb97c5f8a6113495662e0f70c565
mayrasillas <mayrasillas.c@hotmail.com>
2021-09-14 11:19:15 -0500

    moficacion database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,45 +1,24 @@
 module.exports = {
 
     "development": {
-    "username": "b2f0d88d19bdca",
-    "password": "85e0f270",
-    "database": "heroku_4d9c94641d2f658",
-    "host": "us-cdbr-east-04.cleardb.com",
+    "username": "root",
+    "password": "mj*Cp-YwV9!d%BY",
+    "database": "bits",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "b2f0d88d19bdca",
-    "password": "85e0f270",
-    "database": "heroku_4d9c94641d2f658",
-    "host": "us-cdbr-east-04.cleardb.com",
+    "username": "root",
+    "password": "mj*Cp-YwV9!d%BY",
+    "database": "bits",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "b2f0d88d19bdca",
-    "password": "85e0f270",
-    "database": "heroku_4d9c94641d2f658",
-    "host": "us-cdbr-east-04.cleardb.com",
+    "username": "root",
+    "password": "mj*Cp-YwV9!d%BY",
+    "database": "bits",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   }
- /* "development": {
-    "username": "root",
-    "password": "mj*Cp-YwV9!d%BY",
-    "database": "bits",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": "mj*Cp-YwV9!d%BY",
-    "database": "bits",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": "mj*Cp-YwV9!d%BY",
-    "database": "bits",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }*/
 }
