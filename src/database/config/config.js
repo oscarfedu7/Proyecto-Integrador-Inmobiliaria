cb709664e807b717643505ee1f2df1d32fda28f6
mayrasillas <mayrasillas.c@hotmail.com>
2021-09-14 16:13:31 -0500

    confgurando bd con heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,6 +1,25 @@
 module.exports = {
+  "development": {
+    "username": "b1c2a3bc440632",
+    "password": "aa526e76",
+    "database": "heroku_501a33dc4a326ac",
+    "host": "us-cdbr-east-04.cleardb.com",
+  },
+  "test": {
+    "username": "b1c2a3bc440632",
+    "password": "aa526e76",
+    "database": "heroku_501a33dc4a326ac",
+    "host": "us-cdbr-east-04.cleardb.com",
+  },
+  "production": {
+    "username": "b1c2a3bc440632",
+    "password": "aa526e76",
+    "database": "heroku_501a33dc4a326ac",
+    "host": "us-cdbr-east-04.cleardb.com",
+  }   
 
-    "development": {
+
+   /* "development": {
     "username": "root",
     "password": "mj*Cp-YwV9!d%BY",
     "database": "bits",
@@ -19,6 +38,6 @@ module.exports = {
     "password": "mj*Cp-YwV9!d%BY",
     "database": "bits",
     "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    "dialect": "mysql" 
+  } */
 }
