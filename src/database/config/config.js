990bfaf58c92282f776c5befaf5faaab3d9eb4ce
oscarfedu <58915126+oscarfedu@users.noreply.github.com>
2021-10-04 19:56:01 -0500

    Update config.js
    Configurando la base de datos en herocku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,4 @@
 module.exports = {
-  /*"development": {
-    "username": "b1c2a3bc440632",
-    "password": "aa526e76",
-    "database": "heroku_501a33dc4a326ac",
-    "host": "us-cdbr-east-04.cleardb.com",
-  },
-  "test": {
-    "username": "b1c2a3bc440632",
-    "password": "aa526e76",
-    "database": "heroku_501a33dc4a326ac",
-    "host": "us-cdbr-east-04.cleardb.com",
-  },
-  "production": {
-    "username": "b1c2a3bc440632",
-    "password": "aa526e76",
-    "database": "heroku_501a33dc4a326ac",
-    "host": "us-cdbr-east-04.cleardb.com",
-  }   */
-
-
     "development": {
     "username": "root",
     "password": "mj*Cp-YwV9!d%BY",
@@ -33,11 +13,11 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": "mj*Cp-YwV9!d%BY",
-    "database": "bits",
-    "host": "127.0.0.1",
-    "dialect": "mysql" 
-  } 
+    "production": {
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
+    "dialect": "mysql"
+  }
 }
