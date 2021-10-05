009c159385fd50438a548de361a93bf7ed94c4e4
oscarfedu <oscarfd@live.com.mx>
2021-10-04 19:48:57 -0500

    Configurando herocku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "oscar",
-    "password": "root",
-    "database": "bits",
-    "host": "127.0.0.1",
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
+//mysql://b2f0d88d19bdca:85e0f270@us-cdbr-east-04.cleardb.com/heroku_4d9c94641d2f658?reconnect=true
