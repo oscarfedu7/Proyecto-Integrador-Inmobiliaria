ff2f1929127caf210503606b6a0bd23b388c458a
Mayra Vanessa Sillas Cavazos <84360847+mayrasillas@users.noreply.github.com>
2021-10-06 00:59:27 -0500

    Add files via upload

---
+++ public/js/scripts.js
@@ -0,0 +1,12 @@
+jQuery('document').ready(function($){
+    var menuBtn = $('.menu-icon'),
+     menu = $('.navegation ul'); 
+     menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }
+        else{
+            menu.addClass('show');
+        }
+     });
+});
