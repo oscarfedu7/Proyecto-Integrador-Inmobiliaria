f966daef4f394114bd12a29b8f6656c01d5c387c
oscarfedu <oscarfd@live.com.mx>
2021-10-07 20:40:11 -0500

    Modificando el config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "oscar",
-    "password": "root",
-    "database": "bits",
-    "host": "127.0.0.1",
+    "username": "b2f0d88d19bdca",
+    "password": "85e0f270",
+    "database": "heroku_4d9c94641d2f658",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
